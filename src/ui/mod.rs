9c7135b541dea99930bb0d9a02f4097df9fc84fd
syuya2036 <shuya@yantan.dev>
2025-11-11 12:23:37 +0900

    Rename package and related references from "nohr" to "nohrs"

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -6,6 +6,6 @@ pub mod theme;
 pub mod window;
 
 // Public UI entry points that don't pull external UI toolkits yet.
-pub use app::NohrApp;
+pub use app::NohrsApp;
 pub mod components;
 pub use components::file_list;
